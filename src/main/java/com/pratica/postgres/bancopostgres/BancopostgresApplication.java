4ba8ccd59e836532e2816dd868cc0a69d5b19838
Lucas Santana Duarte <45908363+lucasduartedev@users.noreply.github.com>
2021-09-20 10:04:08 -0300

    Definição do projeto

---
+++ src/main/java/com/pratica/postgres/bancopostgres/BancopostgresApplication.java
@@ -0,0 +1,13 @@
+package com.pratica.postgres.bancopostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancopostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancopostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratica/postgres/bancopostgres/BancopostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica.postgres.bancopostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancopostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
